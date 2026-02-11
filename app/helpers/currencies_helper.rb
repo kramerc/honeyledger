2cfad2af97533bf930a2fbd810a4c0d240cf26d7
Kramer Campbell <kramer@kramerc.com>
2026-02-11 06:50:23 +0000

    Use BigDecimal in amount_to_currency helper

--- app/helpers/currencies_helper.rb
+++ app/helpers/currencies_helper.rb
@@ -1,6 +1,6 @@
 module CurrenciesHelper
   def amount_to_currency(amount_minor, currency)
-    number_to_currency(amount_minor.to_f / (10 ** currency.decimal_places), unit: currency.symbol, precision: currency.decimal_places)
+    number_to_currency(BigDecimal(amount_minor) / (10 ** currency.decimal_places), unit: currency.symbol, precision: currency.decimal_places)
   end
 
   def amount_minor_to_decimal(amount_minor, currency)
