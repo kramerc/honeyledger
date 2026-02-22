baf3356d1e926d26b53d57874695d32c25770e52
Kramer Campbell <kramer@kramerc.com>
2026-02-22 12:02:05 -0800

    Replace importmap with Vite

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import path from "path";
+import { defineConfig } from "vite";
+import RubyPlugin from "vite-plugin-ruby";
+import StimulusHMRPlugin from "vite-plugin-stimulus-hmr";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      // Stimulus controllers
+      "controllers": path.resolve(__dirname, "app/javascript/controllers"),
+    },
+  },
+  plugins: [RubyPlugin(), StimulusHMRPlugin()],
+});
